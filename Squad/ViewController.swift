c5d7c3d2897bfb4eb3b8f0943b458c5a3524aba0
Michael Vieth <mavieth@gmail.com>
2015-12-03 21:43:53 -0600

    Removing test folders.

--- Squad/ViewController.swift
+++ Squad/ViewController.swift
@@ -15,8 +15,7 @@ class ViewController: UIViewController {
         
         override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-            
+            // Do any additional setup after loading the view, typically from a nib.   
         }
         
         override func viewDidAppear(animated: Bool) {
