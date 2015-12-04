c9e7019a652a0c76f4b557f1918bc770d41399ea
Michael Vieth <mavieth@gmail.com>
2015-12-03 19:40:09 -0600

    Not sure what I am doing here.

--- Squad/ViewController.swift
+++ Squad/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // works
         // .gitignore.
+        // third
+        
     }
 
     override func didReceiveMemoryWarning() {
